c44f19c5f3b27136b4427b8543af359ac28d8b00
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-08-05 16:01:36 +0100

    variables changed info

---
+++ variables.js
@@ -0,0 +1,60 @@
+// let i = 10;
+
+// i += 2;
+
+// console.log(i)
+
+// let favouriteDrink = "coffee";
+
+// console.log("my favourite drink is " + favouriteDrink);
+
+
+// let name = 'kris';
+// let age = '41';
+// let favDrink = 'coffee';
+
+// console.log(`Hi. my name is ${name} . my age is  ${age}  and my favorite drink is  ${favDrink}`)
+
+// age = 60;
+// favDrink = "pepsi"
+
+// console.log(`Hi. my name is ${name} . my age is  ${age}  and my favorite drink is  ${favDrink}`)
+
+// activity1
+// let name = 'Kris';
+// let age = 41;
+// let favColour = 'red';
+
+// console.log(`Hi. my name is ${name} . my age is  ${age}  and my favorite drink is  ${favColour}`)
+
+
+//  let name = 'Kris';
+//  let age = 41;
+//  let favColour = 'red';
+
+// console.log(`Hi my name is ${name}. my age is  ${age}  and my favorite drink is  ${favColour}`)
+
+// name = 'john';
+// favColour = 'purple';
+// age = 45;
+
+//console.log(`Hi. my name is ${name} . my age is  ${age}  and my favorite drink is  ${favColour}`)
+
+
+
+// activity2
+
+
+let breakfast = 'cornflakes';
+let lunch = 'subway';
+let dinner = 'pasta';
+
+console.log(`For breakfast i eat ${breakfast}  for lunch i eat ${lunch} and for dinner i will eat 
+    ${dinner}`)
+
+  breakfast = 'toast';
+  lunch = 'kfc'
+  dinner = 'cottage pie'
+
+
+    console.log(`For breakfast  tomorrow i will eat ${breakfast}  for lunch tomorrow i will eat ${lunch} and for dinner tomorrow i will eat ${dinner}`)  
