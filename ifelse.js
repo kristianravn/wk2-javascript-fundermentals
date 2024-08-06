23abdb98616d71466d2ad8609b326ecdd054c5e8
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-08-06 11:11:46 +0100

    if statements

---
+++ ifelse.js
@@ -0,0 +1,30 @@
+// let weather = "sunny";
+
+// if (weather == "sunny") {
+//     console.log("suncream") 
+// } else if (weather == "rainy") {
+// console.log("brolly")
+// } else {
+//     console.log("either way")
+// };
+// console.log("rainy")
+
+// let diceNum = Math.floor(Math.random()*6);
+// console.log(`you rolled a ${diceNum}`)
+
+// if(diceNum >= 4 ){
+//     console.log("win")
+// } else {
+//     console.log??("lose")
+// }
+
+let place = "leeds"
+let weather = "sunny"
+
+if (place =="liverpool" && weather == "sunny") {
+    console.log("check again ....")
+} else if (place == "liverpool" && weather == "rain"){
+    console.log("of course....")
+} else {
+    console.log("what?")
+}
