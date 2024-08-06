6f87bcd375a70a8cbb79ee990f5de98de7379bff
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-08-06 12:20:36 +0100

    new code

--- ifelse.js
+++ ifelse.js
@@ -18,13 +18,48 @@
 //     console.log??("lose")
 // }
 
-let place = "leeds"
-let weather = "sunny"
+// let place = "leeds"
+// let weather = "sunny"
 
-if (place =="liverpool" && weather == "sunny") {
-    console.log("check again ....")
-} else if (place == "liverpool" && weather == "rain"){
-    console.log("of course....")
-} else {
-    console.log("what?")
+// if (place =="liverpool" && weather == "sunny") {
+//     console.log("check again ....")
+// } else if (place == "liverpool" && weather == "rain"){
+//     console.log("of course....")
+// } else {
+//     console.log("what?")
+// }
+
+
+// let car = "peugeot";
+
+// if(car == "ford" || car )
+
+let team = "everton";
+
+switch(team){
+    case "ManUtd":
+    case "chelsea":
+    case "everton":
+        console.log("you are rubbish teams");
+        break;
+    case "arsenal":
+    case "leedsUtd":  
+         break;      
+        default:
+        console.log("liverpool is number 1!!!");            
 }
+
+
+// let age = 21;
+
+// if (age < 18) {
+//     console.log('too young')
+// } else {
+//     console.log('old enough')
+// }
+
+// let age = 19;
+
+// let canDrink = (age < 18) ? 'too young' : 'old enough'
+
+// console.log(canDrink)
