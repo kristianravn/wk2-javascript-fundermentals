ae319ca8ce6b7f9cac8281e06e2c840b33cb3725
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-08-06 15:15:37 +0100

    loops

--- ifelse.js
+++ ifelse.js
@@ -34,28 +34,28 @@
 
 // if(car == "ford" || car )
 
-let team = "everton";
+// let team = "Liverpool";
 
-switch(team){
-    case "ManUtd":
-    case "chelsea":
-    case "everton":
-        console.log("you are rubbish teams");
-        break;
-    case "arsenal":
-    case "leedsUtd":  
-         break;      
-        default:
-        console.log("liverpool is number 1!!!");            
-}
+// switch(team){
+//     case "ManUtd":
+//     case "chelsea":
+//     case "everton":
+//         console.log("you are rubbish teams");
+//         break;
+//     case "arsenal":
+//     case "leedsUtd":  
+//          break;      
+//         default:
+//         console.log("liverpool is number 1!!!");            
+// }
 
 
 // let age = 21;
 
-// if (age < 18) {
-//     console.log('too young')
+// if (age > 17) {
+//     console.log('i can serve you')
 // } else {
-//     console.log('old enough')
+//     console.log('not old enough')
 // }
 
 // let age = 19;
@@ -63,3 +63,62 @@ switch(team){
 // let canDrink = (age < 18) ? 'too young' : 'old enough'
 
 // console.log(canDrink)
+
+// let pizzatopping = "chicken";
+
+// switch(pizzatopping){
+//     case 'chicken':
+//          console.log("i dont like ");
+//           break;
+//     case "pepper":
+//         console.log('no thank you')
+//           break;      
+//           default:
+//          console.log("pinapple is number 1!!!"); 
+// }
+
+
+// let num = 20;
+// if (num / 3){
+//     console.log('fizz')
+// } else  (num / 5 )
+//     console.log('buzz')
+
+
+// let coffeeOrder = [
+//     "Alex - Cortado",
+//     "ben - Cortado",
+//     "Charlie - Whats new",
+// ];
+// console.log(coffeeOrder);
+
+
+// let faveSongs = [
+//     "dancing Queen",
+//     "girl On Fire",
+//     "stand by me",
+//     "walking on sunshine"
+// ];
+
+// for (let i = 0; i <  faveSongs.length; i++){
+//     console.log(faveSongs[i]);
+// }
+
+
+// let multiplesTwo = [];
+
+// for (let i = 0; i < 21; i++){
+//     if (i % 2 == 0){
+//         multiplesTwo.push(i);
+//     }
+    
+// }
+// console.log(`Numbers divisible by 2: ${multiplesTwo}`)
+
+
+
+let age = 10;
+while(age < 18 ){
+    console.log("you are a child")
+    age++
+}
