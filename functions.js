9dad742ef1fb6c6be4e1af4ff168a0e5dc4c2618
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-08-07 14:20:01 +0100

    activitys

--- functions.js
+++ functions.js
@@ -1,8 +1,61 @@
 
+// let coffeeIsGrinding = false;
 
-const pressGrindBean = () => {
-    console.log("grinding for 20 seconds");
+// const pressGrindBeans = () => {
+//     if ( coffeeIsGrinding ) {
+//         console.log("stop");
+//     } else {
+//         console.log("grinding beans for 20 seconds");
+//         coffeeIsGrinding = true;
+//     }
+// }
 
+// pressGrindBeans();
+
+
+
+// const favcolor = () => {
+//     console.log("my favorite color is red")
+// }
+
+// favcolor()
+
+// parameters
+
+// function square (number){
+//     return number * number;
+// };
+
+//  square (5);
+
+// activity1
+// const factorial = (n) => {
+//     if ((n === 0) || (n === 1)) {
+//         return 1;
+//     } else {
+//         return (n * factorial(n-1));
+//     }
+// }
+
+// console.log(factorial(33));
+
+
+// activity2
+// let orderCounter = 0;
+// const takeOrder = (topping1, topping2, topping3) => {
+//     console.log(`can i have a pizza with ${topping1} & ${topping2} & ${topping3}`);
+//         orderCounter++}
+
+// takeOrder("chicken", "sweetcorn", "pineapple")
+
+// activity 3
+
+let accnumber = 5044992;
+
+const cashWithdrawal = (amount, accnum) => {
+
+    console.log(`withdrawing ${amount} from account ${accnum}`);
 }
 
-pressGrindBeans();
+cashWithdrawal(300, accnumber);
+cashWithdrawal(30, 5049921);
